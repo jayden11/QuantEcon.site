ba5e73bcfd395adf0395a3061895bec44f4c9ad7
John Stachurski <john.stachurski@gmail.com>
2014-11-06 17:22:49 +1100

    Added upload file

---
+++ upload.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os
+
+localdir = "./"
+remotedir = "~/public_html/beta.quantecon.org/"
+server = "chickenlegs.org"
+user = "chickenl"
+
+flag = raw_input("Run as test or run for real? (r/t): ")
+
+assert flag == 'r' or flag == 't', "Unrecognized option."
+
+if flag == 't':
+    options = "-avzun -e ssh --delete"
+else:
+    options = "-avzu -e ssh --delete"
+
+command_list = (options, localdir, user, server, remotedir)
+print "Executing rsync %s %s %s@%s:%s" % command_list
+os.system("rsync %s %s %s@%s:%s" % command_list)
+
