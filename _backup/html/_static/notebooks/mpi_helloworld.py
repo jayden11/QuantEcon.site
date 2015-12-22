caf4c818e2ea24b874986fb8d5de152f970a3a5a
Matt McKay <mamckay@gmail.com>
2015-12-22 12:23:05 -0500

    Removing _backup/ files as they do not require version tracking, updated git ignore file

--- _backup/html/_static/notebooks/mpi_helloworld.py
+++
@@ -1,8 +0,0 @@
-
-from mpi4py import MPI
-
-comm = MPI.COMM_WORLD #retreive the communicator module
-rank = comm.Get_rank() #get the rank of the process
-size = comm.Get_size() #get the number of processes
-
-print "Hello world from process "+str(rank)+" of " + str(size)
